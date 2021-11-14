e3d98e0e64a4bfe310bdde68efb3ee65ac480e12
Robertson <Robertson.Melissa1@mayo.edu>
2021-11-14 16:38:42 -0600

    deliverable 2 done

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -31,3 +31,23 @@ into retiring_titles
 from unique_titles
 group by title
 order by count DESC;
+
+-----------------------------------------------------------------------------------------------------------------------
+--Deliverable 2
+---------------------------------------------------------------------------------------------------------------------------
+
+select distinct on(e.emp_no) e.emp_no,
+e.first_name,
+e.last_name,
+e.birth_date,
+de.from_date,
+de.to_date,
+t.title
+into mentor_eligible
+from employees as e
+left join dept_emp as de on (e.emp_no= de.emp_no)
+left join titles as t on (e.emp_no=t.emp_no)
+where (de.to_date='9999-01-01') and (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no, t.to_date DESC
+
+
